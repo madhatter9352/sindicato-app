8b56c3779b3a5dcab6273d5893ce722a467f2ad2
madhatter9352 <51685059+madhatter9352@users.noreply.github.com>
2022-11-06 22:05:32 -0400

    comentando login para gladys

--- src/modules/main/header/user-dropdown/UserDropDown.js
+++ src/modules/main/header/user-dropdown/UserDropDown.js
@@ -7,13 +7,17 @@ import {logout} from "../../../../store/reducers/auth";
 
 export const UserDropDown = () => {
     const dispatch = useDispatch();
-    const {user} = useSelector(state => state.auth);
+    //const {user} = useSelector(state => state.auth);
     const handleLogout = () => {
         dispatch(logout())
     }
     return (
         <div className='nav-link' style={{paddingLeft: '0'}}>
-            <Dropdown pointing='top left' text={user.first_name + ' ' + user.last_name}>
+            <Dropdown 
+                pointing='top left' 
+                //text={user.first_name + ' ' + user.last_name}
+                text='Jhon doe'
+            >
                 <Dropdown.Menu direction='left'>
                     <Dropdown.Item as={Link} to={`/`} text='My Profile' icon='user'/>
                     <Dropdown.Item text='Logout' icon='power' onClick={handleLogout}/>
